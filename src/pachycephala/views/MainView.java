002511aa1256ae9372fb33b8dc621968fab9fa27
Sitatunga147 <elliot.wymore@gmail.com>
2025-06-23 11:04:01 -0500

    Add the tabs
    Set the content pane of MainView to a tabbedPane with Players and Tournaments tabs.

--- src/pachycephala/views/MainView.java
+++ src/pachycephala/views/MainView.java
@@ -4,12 +4,20 @@ import javax.swing.*;
 
 @SuppressWarnings("serial")
 public class MainView extends JFrame {
+	private JTabbedPane tabbedPane;
+	private JPanel playersPanel;
+	private JPanel tournamentsPanel;
 	
 	public MainView() {
 		setTitle("Pachycephala Tournament Manager");
 		setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		
+		tabbedPane = new JTabbedPane();
+		tabbedPane.addTab("Players", playersPanel);
+		tabbedPane.addTab("Tournaments", tournamentsPanel);
+		setContentPane(tabbedPane);
 	}
 	
 }
